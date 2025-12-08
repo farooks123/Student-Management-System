2c733b4657c99d4213392d11b16fd34472f9baab
farooks123 <umarfarookvj@gmail.com>
2025-12-08 18:49:57 +0530

    update

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,28 @@
-const express=require("express")
+const express=require("express");
+const ConnectDB = require("./connection/db");
 const app=express()// calling the function
-
+const mongoose=require("mongoose");
+const Student=require("./model/Student")
 const PORT=3000;//port should be used 
 
+
+ConnectDB();
+
+app.post("api/student",async(req,res)=>{
+
+    try {
+       const newStudent=req.body;
+       const student=new Student(newStudent);
+       await student.save();
+       res.status(200).json({
+        success:true,
+        message:"Successfullly Created Student",
+        student:student,
+       })
+    } catch (error) {
+        console.log("Error creating Student",error)
+    }
+});
 app.listen(PORT,()=>{
     console.log("Application is running on PORT",PORT);
 })
